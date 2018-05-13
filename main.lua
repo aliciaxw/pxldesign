2b15ef0dd4fcdd59841febf4fd9885390cbf11f1
aliciaxw <alicia.xinran.wang@gmail.com>
2018-05-13 16:49:31 -0400

    switched to storing using a 2D array

--- main.lua
+++ main.lua
@@ -1,40 +1,43 @@
--- TODO: avoid duplicate points in pixels
--- TODO: how to check if out of window 
-
-function table.contains(tabl, val)
-    for i,v in ipairs(tabl) do 
-        if v==val then return true end
-    end
-    return false
-end
+-- TODO: create layer system 
+-- TODO: set up a GUI
+-- TODO: drawing smooth lines 
+-- TODO: figure out a zoom system 
 
 function love.load()
     love.graphics.setBackgroundColor(255,255,255)
-    pixels = {} -- pixels to draw 
+    -- initialize 2D pixel array, empty at first
+    pixels = {}
+    for i = 1,love.graphics.getWidth() do
+        pixels[i] = {}
+        for j = 1,love.graphics.getHeight() do 
+            pixels[i][j] = nil
+        end
+    end 
     canvas = love.graphics.newCanvas(love.graphics.getWidth(), love.graphics.getHeight())
-    love.graphics.setColor(0,0,0)
 end
 
 function love.update(dt)
     pos = {love.mouse.getX(), love.mouse.getY()}
-    if (love.mouse.isDown(1) and 
-        not table.contains(pixels,pos) and                      -- duplicate control
-        pos[1]>=0 and pos[1]<=love.graphics.getWidth() and      -- not off screen
-        pos[2]>=0 and pos[2]<=love.graphics.getHeight()) then
-        table.insert(pixels, pos) -- add to draw!
+    if (love.mouse.isDown(1)) then -- duplicate control
+        pixels[pos[1]][pos[2]] = {0,0,0} -- set pixel to {r, g, b}
     end
-end 
+end
 
 function love.draw()
-    love.graphics.setPointSize(3)
-    if #pixels > 0 then
-        love.graphics.setCanvas(canvas)
-        for i,v in ipairs(pixels) do 
-            love.graphics.points(v[1], v[2]) -- draws the pixels as pts
+    love.graphics.setPointSize(1)
+    -- setting canvas
+    love.graphics.setCanvas(canvas)
+    -- draw points 
+    for i = 1,love.graphics.getWidth() do
+        for j = 1,love.graphics.getHeight() do
+            if pixels[i][j] ~= nil then 
+                -- set color, then draw point
+                love.graphics.setColor(pixels[i][j][1], pixels[i][j][2], pixels[i][j][3])
+                love.graphics.points(i,j)
+            end
         end
-        love.graphics.setCanvas() 
     end
+    love.graphics.setCanvas()
     love.graphics.draw(canvas,0,0)
-    love.graphics.print("How many pixels in table: " .. #pixels, 0,0)
     love.graphics.print("Mouse cursor position: "..pos[1].." "..pos[2], 0, 15)
 end
